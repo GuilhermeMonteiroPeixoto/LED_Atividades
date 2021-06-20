eef160b965d869e96251fe1ca83cb83c4a8fe8a9
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2021-06-20 16:23:07 -0300

    Create vacinacaocovid_estabelecimento.sql

---
+++ Parte_2/ProjectFolder_Dump_Structure_Only/vacinacaocovid_estabelecimento.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE  IF NOT EXISTS `vacinacaocovid` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `vacinacaocovid`;
+-- MariaDB dump 10.19  Distrib 10.4.19-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: vacinacaocovid
+-- ------------------------------------------------------
+-- Server version	10.4.10-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `estabelecimento`
+--
+
+DROP TABLE IF EXISTS `estabelecimento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `estabelecimento` (
+  `estabelecimento_valor` int(11) NOT NULL,
+  `estabelecimento_razaosocial` varchar(70) COLLATE utf8_bin DEFAULT NULL,
+  `estalecimento_nofantasia` varchar(70) COLLATE utf8_bin DEFAULT NULL,
+  `estabelecimento_municipio_codigo` int(11) DEFAULT NULL,
+  `estabelecimento_municipio_nome` varchar(70) COLLATE utf8_bin DEFAULT NULL,
+  `estabelecimento_uf` varchar(5) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`estabelecimento_valor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-20 16:08:22
